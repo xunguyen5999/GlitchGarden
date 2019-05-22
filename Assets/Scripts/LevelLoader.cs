a28c9749469017baf77cc0bd2525b86e87717507
xunguyen5999 <39289855+xunguyen5999@users.noreply.github.com>
2019-05-21 19:22:54 -0700

    Add files via upload

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int waitTime = 6;
+    int currentScenceIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScenceIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentScenceIndex == 0) {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime() {
+        yield return new WaitForSeconds(waitTime);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene() {
+        SceneManager.LoadScene(currentScenceIndex + 1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
